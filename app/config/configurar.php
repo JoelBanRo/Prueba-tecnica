d6d2ff359819f8815fc17b1988a9be87be75af5e
JoelBanRo <80085207+JoelBanRo@users.noreply.github.com>
2023-04-16 22:47:33 -0500

    Update configurar.php

--- app/config/configurar.php
+++ app/config/configurar.php
@@ -4,7 +4,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USUARIO', 'root');
 define('DB_PASSWORD', '');
-define('DB_NOMBRE', 'proyecto_joel2');
+define('DB_NOMBRE', 'prueba-tecnica');
 
 // Ruta de la aplicacion 
 define('RUTA_APP', dirname(dirname(__FILE__))) ;
